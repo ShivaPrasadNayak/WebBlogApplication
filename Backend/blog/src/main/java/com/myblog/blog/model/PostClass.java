7155283f9a43020cc84082e3064dd6bfa1854b67
ShivaPrasadNayak <shivaprasadnayak2404@gmail.com>
2024-01-02 23:41:38 +0530

    removed manually added hibernate from pom and added setters and getters

--- Backend/blog/src/main/java/com/myblog/blog/model/PostClass.java
+++ Backend/blog/src/main/java/com/myblog/blog/model/PostClass.java
@@ -13,12 +13,36 @@ public class PostClass {
     private Long id;
 
     private String title;
-    //private String content;
-    //private String author;
+    private String content;
+    private String author;
     // ... other properties
     
     
-    public PostClass() {
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public PostClass() {
     	
     }
 
